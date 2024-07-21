81e4f8efef4196a257c54db54028e1cd781dec56
m.kosek <m.kosek@netsystem.cz>
2024-07-21 14:02:56 +0200

    show mainpage and edited navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,17 @@
 import 'package:bakaplay/components/navbar.dart';
 import 'package:bakaplay/components/theme_switch_tile.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
-  final user = FirebaseAuth.instance.currentUser!;
+  //final user = FirebaseAuth.instance.currentUser!;
 
   // sign user out method
-  void signUserOut() {
+  /*void signUserOut() {
     FirebaseAuth.instance.signOut();
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -19,19 +19,19 @@ class HomePage extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.surface,
-        actions: [
-          IconButton(
+        actions: const [
+          /*IconButton(
             onPressed: signUserOut,
             icon: const Icon(Icons.logout),
             color: Theme.of(context).colorScheme.onSurface,
-          )
+          )*/
         ],
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            /*Text(
               "LOGGED IN",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Theme.of(context).colorScheme.onSurface),
             ),
@@ -43,13 +43,13 @@ class HomePage extends StatelessWidget {
               style: TextStyle(fontSize: 20, color: Theme.of(context).colorScheme.onSurface),
             ),
 
-            const SizedBox(height: 10),
+            const SizedBox(height: 10),*/
 
-            const ThemeSwitchTile()
+            ThemeSwitchTile()
           ],
         ),
       ),
-      bottomNavigationBar: Navbar(), // Use the Navbar component here
+      bottomNavigationBar: const Navbar(),
     );
-  } //test commit
+  }
 }
